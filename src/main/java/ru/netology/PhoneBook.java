bea2087d19c062c261f00d1c1d2bcdba3eb4be71
Nikolay Golovin <iimonolitii@mail.ru>
2024-10-14 13:09:08 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,10 @@
 package ru.netology;
 
 public class PhoneBook {
+    // Заглушка для метода add
+    public int add(String name, String number) {
+        return 0;
+    }
+
+    // Другие методы будут добавлены позже
 }
--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,4 +1,13 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int count = phoneBook.add("Иван Иванов", "1234567890");
+        assertEquals(1, count, "Количество контактов должно быть 1 после добавления одного контакта");
+    }
 }
