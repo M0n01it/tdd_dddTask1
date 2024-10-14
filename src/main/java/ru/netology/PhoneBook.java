455f81dbad9379715b502e8f9461bb7fda9c11b0
Nikolay Golovin <iimonolitii@mail.ru>
2024-10-14 13:13:10 +0300

    findByNumber implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -2,20 +2,26 @@ package ru.netology;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.HashMap;
 
 public class PhoneBook {
     private Map<String, String> contacts;
+    private Map<String, String> numberToName;
 
     public PhoneBook() {
         contacts = new TreeMap<>();
+        numberToName = new HashMap<>();
     }
 
     public int add(String name, String number) {
         contacts.put(name, number);
+        numberToName.put(number, name);
         return contacts.size();
     }
 
     public String findByNumber(String number) {
-        return null;
+        return numberToName.get(number);
     }
+
+    // Другие методы будут добавлены позже
 }
